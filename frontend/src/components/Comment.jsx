95bc16a8849174892acc6084029526fa939b204a
Hồng Nguyên <95576426+hongnguyen2011@users.noreply.github.com>
2024-03-19 22:50:52 +0700

    Post page added

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,37 +1,26 @@
 import { Avatar, Divider, Flex, Text } from "@chakra-ui/react";
-import { useState } from "react";
-import { BsThreeDots } from "react-icons/bs";
-import Actions from "./Actions";
 
-const Comment = ({ userAvatar, createdAt, comment, username, likes }) => {
-	const [liked, setLiked] = useState(false);
-
-	return (
-		<>
-			<Flex gap={4} py={2} my={2} w={"full"}>
-				<Avatar src={userAvatar} size={"sm"} />
-				<Flex gap={1} w={"full"} flexDirection={"column"}>
-					<Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
-						<Text fontSize='sm' fontWeight='bold'>
-							{username}
-						</Text>
-						<Flex gap={2} alignItems={"center"}>
-							<Text fontSize={"sm"} color={"gray.light"}>
-								{createdAt}
-							</Text>
-							<BsThreeDots />
-						</Flex>
-					</Flex>
-					<Text>{comment}</Text>
-					<Actions liked={liked} setLiked={setLiked} />
-					<Text fontSize={"sm"} color={"gray.light"}>
-						{likes + (liked ? 1 : 0)} likes
-					</Text>
-				</Flex>
-			</Flex>
-			<Divider />
-		</>
-	);
+const Comment = ({ reply, lastReply }) => {
+  return (
+    <>
+      <Flex gap={4} py={2} my={2} w={"full"}>
+        <Avatar src={reply.userProfilePic} size={"sm"} />
+        <Flex gap={1} w={"full"} flexDirection={"column"}>
+          <Flex
+            w={"full"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <Text fontSize="sm" fontWeight="bold">
+              {reply.username}
+            </Text>
+          </Flex>
+          <Text>{reply.text}</Text>
+        </Flex>
+      </Flex>
+      {!lastReply ? <Divider /> : null}
+    </>
+  );
 };
 
 export default Comment;
--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,84 +1,148 @@
-import { Avatar, Box, Button, Divider, Flex, Image, Text } from "@chakra-ui/react";
-import { BsThreeDots } from "react-icons/bs";
+import {
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Image,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import Actions from "../components/Actions";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Comment from "../components/Comment";
+import useGetUserProfile from "../hooks/useGetUserProfile";
+import useShowToast from "../hooks/useShowToast";
+import { useNavigate, useParams } from "react-router-dom";
+import { formatDistanceToNow } from "date-fns";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { DeleteIcon } from "@chakra-ui/icons";
 
 const PostPage = () => {
-	const [liked, setLiked] = useState(false);
-	return (
-		<>
-			<Flex>
-				<Flex w={"full"} alignItems={"center"} gap={3}>
-					<Avatar src='/zuck-avatar.png' size={"md"} name='Mark Zuckerberg' />
-					<Flex>
-						<Text fontSize={"sm"} fontWeight={"bold"}>
-							markzuckerberg
-						</Text>
-						<Image src='/verified.png' w='4' h={4} ml={4} />
-					</Flex>
-				</Flex>
-				<Flex gap={4} alignItems={"center"}>
-					<Text fontSize={"sm"} color={"gray.light"}>
-						1d
-					</Text>
-					<BsThreeDots />
-				</Flex>
-			</Flex>
+  const { user, loading } = useGetUserProfile();
+  const [post, setPost] = useState(null);
+  const showToast = useShowToast();
+  const { pid } = useParams();
+  const currentUser = useRecoilValue(userAtom);
+  const navigate = useNavigate();
 
-			<Text my={3}>Let&apos;s talk about Threads.</Text>
+  useEffect(() => {
+    const getPost = async () => {
+      try {
+        const res = await fetch(`/api/posts/${pid}`);
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        console.log(data);
+        setPost(data);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      }
+    };
+    getPost();
+  }, [showToast, pid]);
 
-			<Box borderRadius={6} overflow={"hidden"} border={"1px solid"} borderColor={"gray.light"}>
-				<Image src={"/post1.png"} w={"full"} />
-			</Box>
+  const handleDeletePost = async () => {
+    try {
+      if (!window.confirm("Are you sure you want to delete this post?")) return;
 
-			<Flex gap={3} my={3}>
-				<Actions liked={liked} setLiked={setLiked} />
-			</Flex>
+      const res = await fetch(`/api/posts/${post._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.error) {
+        showToast("Error", data.error, "error");
+        return;
+      }
+      showToast("Success", "Post deleted", "success");
+      navigate(`/${user.username}`);
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    }
+  };
 
-			<Flex gap={2} alignItems={"center"}>
-				<Text color={"gray.light"} fontSize={"sm"}>
-					238 replies
-				</Text>
-				<Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
-				<Text color={"gray.light"} fontSize={"sm"}>
-					{200 + (liked ? 1 : 0)} likes
-				</Text>
-			</Flex>
-			<Divider my={4} />
+  if (!user && loading) {
+    return (
+      <Flex justifyContent={"center"}>
+        <Spinner size={"xl"} />
+      </Flex>
+    );
+  }
 
-			<Flex justifyContent={"space-between"}>
-				<Flex gap={2} alignItems={"center"}>
-					<Text fontSize={"2xl"}>👋</Text>
-					<Text color={"gray.light"}>Get the app to like, reply and post.</Text>
-				</Flex>
-				<Button>Get</Button>
-			</Flex>
+  if (!post) return null;
 
-			<Divider my={4} />
-			<Comment
-				comment='Looks really good!'
-				createdAt='2d'
-				likes={100}
-				username='johndoe'
-				userAvatar='https://bit.ly/dan-abramov'
-			/>
-			<Comment
-				comment='Amazing!'
-				createdAt='1d'
-				likes={21}
-				username='janedoe'
-				userAvatar='https://bit.ly/code-beast'
-			/>
-			<Comment
-				comment='Looks good!'
-				createdAt='2d'
-				likes={42}
-				username='sallydoe'
-				userAvatar='https://bit.ly/sage-adebayo'
-			/>
-		</>
-	);
+  return (
+    <>
+      <Flex>
+        <Flex w={"full"} alignItems={"center"} gap={3}>
+          <Avatar src={user.profilePic} size={"md"} name="Mark Zuckerberg" />
+          <Flex>
+            <Text fontSize={"sm"} fontWeight={"bold"}>
+              {user.username}
+            </Text>
+            <Image src="/verified.png" w="4" h={4} ml={4} />
+          </Flex>
+        </Flex>
+        <Flex gap={4} alignItems={"center"}>
+          <Text
+            fontSize={"xs"}
+            width={36}
+            textAlign={"right"}
+            color={"gray.light"}
+          >
+            {formatDistanceToNow(new Date(post.createdAt))} ago
+          </Text>
+
+          {currentUser?._id === user._id && (
+            <DeleteIcon
+              size={20}
+              cursor={"pointer"}
+              onClick={handleDeletePost}
+            />
+          )}
+        </Flex>
+      </Flex>
+
+      <Text my={3}>{post.text}</Text>
+
+      {post.img && (
+        <Box
+          borderRadius={6}
+          overflow={"hidden"}
+          border={"1px solid"}
+          borderColor={"gray.light"}
+        >
+          <Image src={post.img} w={"full"} />
+        </Box>
+      )}
+
+      <Flex gap={3} my={3}>
+        <Actions post={post} />
+      </Flex>
+
+      <Divider my={4} />
+
+      <Flex justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text fontSize={"2xl"}>👋</Text>
+          <Text color={"gray.light"}>Get the app to like, reply and post.</Text>
+        </Flex>
+        <Button>Get</Button>
+      </Flex>
+
+      <Divider my={4} />
+      {post.replies.map((reply) => (
+        <Comment
+          key={reply._id}
+          reply={reply}
+          lastReply={reply._id === post.replies[post.replies.length - 1]._id}
+        />
+      ))}
+    </>
+  );
 };
 
 export default PostPage;
