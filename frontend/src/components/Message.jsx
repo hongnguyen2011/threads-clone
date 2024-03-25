69a90ad2d2f614168e02f30010c0d4a65779346a
Hồng Nguyên <95576426+hongnguyen2011@users.noreply.github.com>
2024-03-26 01:29:54 +0700

    feat chat messaging done

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,18 +1,24 @@
 import { Avatar, Flex, Text } from "@chakra-ui/react";
+import { selectedConversationAtom } from "../atoms/messagesAtom";
+import { useRecoilValue } from "recoil";
+import userAtom from "../atoms/userAtom";
 
-const Message = ({ ownMessage }) => {
+const Message = ({ ownMessage, message }) => {
+  const selectedConversation = useRecoilValue(selectedConversationAtom);
+  const user = useRecoilValue(userAtom);
+  console.log(user);
   return (
     <>
       {ownMessage ? (
         <Flex gap={2} alignSelf={"flex-end"}>
           <Text maxW={"350px"} bg={"blue.400"} p={1} borderRadius={"md"}>
-            hello
+            {message.text}
           </Text>
-          <Avatar src="" w="7" h={7} />
+          <Avatar src={user.profilePic} w="7" h={7} />
         </Flex>
       ) : (
         <Flex gap={2}>
-          <Avatar src="" w="7" h={7} />
+          <Avatar src={selectedConversation.userProfilePic} w="7" h={7} />
 
           <Text
             maxW={"350px"}
@@ -21,7 +27,7 @@ const Message = ({ ownMessage }) => {
             borderRadius={"md"}
             color={"black"}
           >
-            Helllooo999
+            {message.text}
           </Text>
         </Flex>
       )}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,39 +8,43 @@ import { extendTheme } from "@chakra-ui/theme-utils";
 import { ColorModeScript } from "@chakra-ui/color-mode";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 const styles = {
-	global: (props) => ({
-		body: {
-			color: mode("gray.800", "whiteAlpha.900")(props),
-			bg: mode("gray.100", "#101010")(props),
-		},
-	}),
+  global: (props) => ({
+    body: {
+      color: mode("gray.800", "whiteAlpha.900")(props),
+      bg: mode("gray.100", "#101010")(props),
+    },
+  }),
 };
 
 const config = {
-	initialColorMode: "dark",
-	useSystemColorMode: true,
+  initialColorMode: "dark",
+  useSystemColorMode: true,
 };
 
 const colors = {
-	gray: {
-		light: "#616161",
-		dark: "#1e1e1e",
-	},
+  gray: {
+    light: "#616161",
+    dark: "#1e1e1e",
+  },
 };
 
 const theme = extendTheme({ config, styles, colors });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<RecoilRoot>
-			<BrowserRouter>
-				<ChakraProvider theme={theme}>
-					<ColorModeScript initialColorMode={theme.config.initialColorMode} />
-					<App />
-				</ChakraProvider>
-			</BrowserRouter>
-		</RecoilRoot>
-	</React.StrictMode>
+  // React.StrictMode renders every component twice (in the initial render), only in development.
+  <React.StrictMode>
+    <RecoilRoot>
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <SocketContextProvider>
+            <App />
+          </SocketContextProvider>
+        </ChakraProvider>
+      </BrowserRouter>
+    </RecoilRoot>
+  </React.StrictMode>
 );
